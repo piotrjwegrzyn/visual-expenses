ae2d690880b00b0aa2c70da10309b8baeaf67cf2
Piotr Węgrzyn <piotr@wegrzyn.dev>
2024-11-17 00:25:16 +0100

    deployment files

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"net/http"
+)
 
 func main() {
-	slog.Info(helloWorld())
+	http.HandleFunc("/", getRoot)
+
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		slog.Error("error starting server", slog.Attr{Key: "error", Value: slog.StringValue(err.Error())})
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(helloWorld()))
 }
 
 func helloWorld() string {
